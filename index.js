278ebe019da1cd36c5e4ce3c4d2e13074831d862
Justin McMurdie <mcmurdiej@gmail.com>
2019-01-21 17:20:29 -0700

    showing options

--- index.js
+++ index.js
@@ -1,19 +1,42 @@
-function openModal() {
-  let dialog = document.querySelector('.myModal')
-  dialog.showModal()
+let modal = document.querySelector('.myModal')
+dialogPolyfill.registerDialog(modal)
+
+let dialogToast = document.querySelector('.toastBlock')
+dialogPolyfill.registerDialog(dialogToast)
+
+let toast = document.querySelector('.normalToast')
+
+let toastModal = document.querySelector('.toastModal')
+dialogPolyfill.registerDialog(toastModal)
+
+function showHideModal() {
+  if (modal.open) {
+    modal.close()
+  } else {
+    modal.showModal()
+  }
 }
 
-function closeModal() {
-  let dialog = document.querySelector('.myModal')
-  dialog.close()
+function showHideDialogToast () {
+  if (dialogToast.open) {
+    dialogToast.close()
+  } else {
+    dialogToast.show()
+  }
 }
 
-function showToast () {
-  let toast = document.querySelector('.myToast')
-  toast.classList.remove('invisible')
+function showHideToast () {
+  if (toast.classList.contains('invisible')) {
+    toast.classList.remove('invisible')
+  } else {
+    toast.classList.add('invisible')
+  }
 }
 
-function closeToast () {
-  let toast = document.querySelector('.myToast')
-  toast.classList.add('invisible')
+function showHideModalToast () {
+  if (toastModal.open) {
+    toastModal.close()
+  } else {
+    toastModal.showModal()
+  }
 }
